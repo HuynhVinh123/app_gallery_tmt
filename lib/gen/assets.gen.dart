e8e61097a513da294528ed269dcaca959952d3b6
Nguyen Van Nam <namnv@tmtco.asia>
2022-09-22 16:36:16 +0700

    Update

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,374 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  $AssetsImagesHistoryGen get history => const $AssetsImagesHistoryGen();
+
+  /// File path: assets/images/img.jpeg
+  AssetGenImage get img => const AssetGenImage('assets/images/img.jpeg');
+
+  /// File path: assets/images/img_sao_ke.png
+  AssetGenImage get imgSaoKe =>
+      const AssetGenImage('assets/images/img_sao_ke.png');
+
+  $AssetsImagesTestGen get test => const $AssetsImagesTestGen();
+}
+
+class $AssetsImagesHistoryGen {
+  const $AssetsImagesHistoryGen();
+
+  /// File path: assets/images/history/IMG_20190418_183630.jpg
+  AssetGenImage get img20190418183630 =>
+      const AssetGenImage('assets/images/history/IMG_20190418_183630.jpg');
+
+  /// File path: assets/images/history/IMG_20191025_102424.jpg
+  AssetGenImage get img20191025102424 =>
+      const AssetGenImage('assets/images/history/IMG_20191025_102424.jpg');
+
+  /// File path: assets/images/history/IMG_20191025_104926.jpg
+  AssetGenImage get img20191025104926 =>
+      const AssetGenImage('assets/images/history/IMG_20191025_104926.jpg');
+
+  /// File path: assets/images/history/IMG_20191025_112658.jpg
+  AssetGenImage get img20191025112658 =>
+      const AssetGenImage('assets/images/history/IMG_20191025_112658.jpg');
+
+  /// File path: assets/images/history/IMG_20191025_175301.jpg
+  AssetGenImage get img20191025175301 =>
+      const AssetGenImage('assets/images/history/IMG_20191025_175301.jpg');
+
+  /// File path: assets/images/history/IMG_20191025_175404.jpg
+  AssetGenImage get img20191025175404 =>
+      const AssetGenImage('assets/images/history/IMG_20191025_175404.jpg');
+
+  /// File path: assets/images/history/IMG_20191026_054740.jpg
+  AssetGenImage get img20191026054740 =>
+      const AssetGenImage('assets/images/history/IMG_20191026_054740.jpg');
+
+  /// File path: assets/images/history/IMG_20191026_055635.jpg
+  AssetGenImage get img20191026055635 =>
+      const AssetGenImage('assets/images/history/IMG_20191026_055635.jpg');
+
+  /// File path: assets/images/history/IMG_20191026_060701.jpg
+  AssetGenImage get img20191026060701 =>
+      const AssetGenImage('assets/images/history/IMG_20191026_060701.jpg');
+
+  /// File path: assets/images/history/IMG_20191026_060721.jpg
+  AssetGenImage get img20191026060721 =>
+      const AssetGenImage('assets/images/history/IMG_20191026_060721.jpg');
+
+  /// File path: assets/images/history/IMG_20191026_084951.jpg
+  AssetGenImage get img20191026084951 =>
+      const AssetGenImage('assets/images/history/IMG_20191026_084951.jpg');
+
+  /// File path: assets/images/history/IMG_20191026_191639.jpg
+  AssetGenImage get img20191026191639 =>
+      const AssetGenImage('assets/images/history/IMG_20191026_191639.jpg');
+
+  /// File path: assets/images/history/IMG_20191027_121826.jpg
+  AssetGenImage get img20191027121826 =>
+      const AssetGenImage('assets/images/history/IMG_20191027_121826.jpg');
+
+  /// File path: assets/images/history/MAY00590.jpg
+  AssetGenImage get may00590 =>
+      const AssetGenImage('assets/images/history/MAY00590.jpg');
+
+  /// File path: assets/images/history/MAY00592.jpg
+  AssetGenImage get may00592 =>
+      const AssetGenImage('assets/images/history/MAY00592.jpg');
+
+  /// File path: assets/images/history/MAY00594.jpg
+  AssetGenImage get may00594 =>
+      const AssetGenImage('assets/images/history/MAY00594.jpg');
+
+  /// File path: assets/images/history/MAY00596.jpg
+  AssetGenImage get may00596 =>
+      const AssetGenImage('assets/images/history/MAY00596.jpg');
+
+  /// File path: assets/images/history/MAY00599.jpg
+  AssetGenImage get may00599 =>
+      const AssetGenImage('assets/images/history/MAY00599.jpg');
+
+  /// File path: assets/images/history/MAY00608.jpg
+  AssetGenImage get may00608 =>
+      const AssetGenImage('assets/images/history/MAY00608.jpg');
+
+  /// File path: assets/images/history/MAY00610.jpg
+  AssetGenImage get may00610 =>
+      const AssetGenImage('assets/images/history/MAY00610.jpg');
+
+  /// File path: assets/images/history/MAY00614.jpg
+  AssetGenImage get may00614 =>
+      const AssetGenImage('assets/images/history/MAY00614.jpg');
+
+  /// File path: assets/images/history/MAY00623.jpg
+  AssetGenImage get may00623 =>
+      const AssetGenImage('assets/images/history/MAY00623.jpg');
+
+  /// File path: assets/images/history/MAY00624.jpg
+  AssetGenImage get may00624 =>
+      const AssetGenImage('assets/images/history/MAY00624.jpg');
+
+  /// File path: assets/images/history/MAY00627.jpg
+  AssetGenImage get may00627 =>
+      const AssetGenImage('assets/images/history/MAY00627.jpg');
+
+  /// File path: assets/images/history/MAY00628.jpg
+  AssetGenImage get may00628 =>
+      const AssetGenImage('assets/images/history/MAY00628.jpg');
+
+  /// File path: assets/images/history/MAY00632.jpg
+  AssetGenImage get may00632 =>
+      const AssetGenImage('assets/images/history/MAY00632.jpg');
+
+  /// File path: assets/images/history/MAY00661.jpg
+  AssetGenImage get may00661 =>
+      const AssetGenImage('assets/images/history/MAY00661.jpg');
+
+  /// File path: assets/images/history/MAY00668.jpg
+  AssetGenImage get may00668 =>
+      const AssetGenImage('assets/images/history/MAY00668.jpg');
+
+  /// File path: assets/images/history/MAY00674.jpg
+  AssetGenImage get may00674 =>
+      const AssetGenImage('assets/images/history/MAY00674.jpg');
+
+  /// File path: assets/images/history/MAY00679.jpg
+  AssetGenImage get may00679 =>
+      const AssetGenImage('assets/images/history/MAY00679.jpg');
+
+  /// File path: assets/images/history/MAY00688.jpg
+  AssetGenImage get may00688 =>
+      const AssetGenImage('assets/images/history/MAY00688.jpg');
+
+  /// File path: assets/images/history/MAY00692.jpg
+  AssetGenImage get may00692 =>
+      const AssetGenImage('assets/images/history/MAY00692.jpg');
+
+  /// File path: assets/images/history/MAY00696.jpg
+  AssetGenImage get may00696 =>
+      const AssetGenImage('assets/images/history/MAY00696.jpg');
+
+  /// File path: assets/images/history/MAY00697.jpg
+  AssetGenImage get may00697 =>
+      const AssetGenImage('assets/images/history/MAY00697.jpg');
+
+  /// File path: assets/images/history/a20160130_180305.jpg
+  AssetGenImage get a20160130180305 =>
+      const AssetGenImage('assets/images/history/a20160130_180305.jpg');
+
+  /// File path: assets/images/history/a20160130_181911.jpg
+  AssetGenImage get a20160130181911 =>
+      const AssetGenImage('assets/images/history/a20160130_181911.jpg');
+
+  /// File path: assets/images/history/a20160130_200229.jpg
+  AssetGenImage get a20160130200229 =>
+      const AssetGenImage('assets/images/history/a20160130_200229.jpg');
+
+  /// File path: assets/images/history/a20160714_205039.jpg
+  AssetGenImage get a20160714205039 =>
+      const AssetGenImage('assets/images/history/a20160714_205039.jpg');
+
+  /// File path: assets/images/history/a20160714_205050.jpg
+  AssetGenImage get a20160714205050 =>
+      const AssetGenImage('assets/images/history/a20160714_205050.jpg');
+
+  /// File path: assets/images/history/a20160715_084222.jpg
+  AssetGenImage get a20160715084222 =>
+      const AssetGenImage('assets/images/history/a20160715_084222.jpg');
+
+  /// File path: assets/images/history/a20160715_084712_HDR.jpg
+  AssetGenImage get a20160715084712HDR =>
+      const AssetGenImage('assets/images/history/a20160715_084712_HDR.jpg');
+
+  /// File path: assets/images/history/a20160715_084718.jpg
+  AssetGenImage get a20160715084718 =>
+      const AssetGenImage('assets/images/history/a20160715_084718.jpg');
+
+  /// File path: assets/images/history/a20160715_084817_HDR.jpg
+  AssetGenImage get a20160715084817HDR =>
+      const AssetGenImage('assets/images/history/a20160715_084817_HDR.jpg');
+
+  /// File path: assets/images/history/a20160715_100207_HDR.jpg
+  AssetGenImage get a20160715100207HDR =>
+      const AssetGenImage('assets/images/history/a20160715_100207_HDR.jpg');
+
+  /// File path: assets/images/history/a20160715_113243.jpg
+  AssetGenImage get a20160715113243 =>
+      const AssetGenImage('assets/images/history/a20160715_113243.jpg');
+
+  /// File path: assets/images/history/a20160715_202721.jpg
+  AssetGenImage get a20160715202721 =>
+      const AssetGenImage('assets/images/history/a20160715_202721.jpg');
+
+  /// File path: assets/images/history/a20160715_204623.jpg
+  AssetGenImage get a20160715204623 =>
+      const AssetGenImage('assets/images/history/a20160715_204623.jpg');
+
+  /// File path: assets/images/history/a20160715_211508.jpg
+  AssetGenImage get a20160715211508 =>
+      const AssetGenImage('assets/images/history/a20160715_211508.jpg');
+
+  /// File path: assets/images/history/a20160715_211510.jpg
+  AssetGenImage get a20160715211510 =>
+      const AssetGenImage('assets/images/history/a20160715_211510.jpg');
+
+  /// File path: assets/images/history/a20160715_211514.jpg
+  AssetGenImage get a20160715211514 =>
+      const AssetGenImage('assets/images/history/a20160715_211514.jpg');
+
+  /// File path: assets/images/history/a20160716_150904.jpg
+  AssetGenImage get a20160716150904 =>
+      const AssetGenImage('assets/images/history/a20160716_150904.jpg');
+
+  /// File path: assets/images/history/a20160716_152248.jpg
+  AssetGenImage get a20160716152248 =>
+      const AssetGenImage('assets/images/history/a20160716_152248.jpg');
+
+  /// File path: assets/images/history/a20160716_165053.jpg
+  AssetGenImage get a20160716165053 =>
+      const AssetGenImage('assets/images/history/a20160716_165053.jpg');
+
+  /// File path: assets/images/history/a20160716_165709.jpg
+  AssetGenImage get a20160716165709 =>
+      const AssetGenImage('assets/images/history/a20160716_165709.jpg');
+
+  /// File path: assets/images/history/a20160716_184043.jpg
+  AssetGenImage get a20160716184043 =>
+      const AssetGenImage('assets/images/history/a20160716_184043.jpg');
+}
+
+class $AssetsImagesTestGen {
+  const $AssetsImagesTestGen();
+
+  /// File path: assets/images/test/a1.jpg
+  AssetGenImage get a1 => const AssetGenImage('assets/images/test/a1.jpg');
+
+  /// File path: assets/images/test/a10.jpg
+  AssetGenImage get a10 => const AssetGenImage('assets/images/test/a10.jpg');
+
+  /// File path: assets/images/test/a11.jpg
+  AssetGenImage get a11 => const AssetGenImage('assets/images/test/a11.jpg');
+
+  /// File path: assets/images/test/a12.jpg
+  AssetGenImage get a12 => const AssetGenImage('assets/images/test/a12.jpg');
+
+  /// File path: assets/images/test/a13.jpg
+  AssetGenImage get a13 => const AssetGenImage('assets/images/test/a13.jpg');
+
+  /// File path: assets/images/test/a14.jpg
+  AssetGenImage get a14 => const AssetGenImage('assets/images/test/a14.jpg');
+
+  /// File path: assets/images/test/a15.jpg
+  AssetGenImage get a15 => const AssetGenImage('assets/images/test/a15.jpg');
+
+  /// File path: assets/images/test/a16.jpg
+  AssetGenImage get a16 => const AssetGenImage('assets/images/test/a16.jpg');
+
+  /// File path: assets/images/test/a2.jpg
+  AssetGenImage get a2 => const AssetGenImage('assets/images/test/a2.jpg');
+
+  /// File path: assets/images/test/a3.jpg
+  AssetGenImage get a3 => const AssetGenImage('assets/images/test/a3.jpg');
+
+  /// File path: assets/images/test/a4.jpg
+  AssetGenImage get a4 => const AssetGenImage('assets/images/test/a4.jpg');
+
+  /// File path: assets/images/test/a5.jpg
+  AssetGenImage get a5 => const AssetGenImage('assets/images/test/a5.jpg');
+
+  /// File path: assets/images/test/a6.jpg
+  AssetGenImage get a6 => const AssetGenImage('assets/images/test/a6.jpg');
+
+  /// File path: assets/images/test/a7.jpg
+  AssetGenImage get a7 => const AssetGenImage('assets/images/test/a7.jpg');
+
+  /// File path: assets/images/test/a8.jpg
+  AssetGenImage get a8 => const AssetGenImage('assets/images/test/a8.jpg');
+
+  /// File path: assets/images/test/a9.jpg
+  AssetGenImage get a9 => const AssetGenImage('assets/images/test/a9.jpg');
+}
+
+class Assets {
+  Assets._();
+
+  static const AssetGenImage builtByJhbBlack =
+      AssetGenImage('assets/built_by_jhb_black.png');
+  static const AssetGenImage builtByJhbWhite =
+      AssetGenImage('assets/built_by_jhb_white.png');
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
